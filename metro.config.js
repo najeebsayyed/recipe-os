63cf33b457eb9615a45e70d1445625dfcfe4d54e
NajeebSayyed <najeebsayyed777@gmail.com>
2026-02-07 23:02:04 +0530

    Added login screen UI with inputs, button and basic styling

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,18 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
-const config = mergeConfig(getDefaultConfig(__dirname), {
-  /* your config */
+const defaultConfig = getDefaultConfig(__dirname);
+
+const config = mergeConfig(defaultConfig, {
+  transformer: {
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  },
+  resolver: {
+    assetExts: defaultConfig.resolver.assetExts.filter(ext => ext !== 'svg'),
+    sourceExts: [...defaultConfig.resolver.sourceExts, 'svg'],
+  },
 });
 
-module.exports = withNativeWind(config, { input: './global.css' });
+module.exports = withNativeWind(config, {
+  input: './global.css',
+});

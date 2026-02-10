642310ea68abdf6cc134d8b1ce6e8ccdc532e431
NajeebSayyed <najeebsayyed777@gmail.com>
2026-02-10 16:13:51 +0530

    implement custom tab bar UI with icons

--- src/navigation/TabNavigator.tsx
+++ src/navigation/TabNavigator.tsx
@@ -1,15 +1,59 @@
-import {} from 'react-native';
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
 import HomeScreen from '../screens/home/HomeScreen';
 import ExploreScreen from '../screens/explore/ExploreScreen';
 import SavedScreen from '../screens/saved/SavedScreen';
 import ProfileScreen from '../screens/profile/ProfileScreen';
 
+import HomeIcon from '../assets/icons/home.svg';
+import ExploreIcon from '../assets/icons/explore.svg';
+import SavedIcon from '../assets/icons/favorite.svg';
+import ProfileIcon from '../assets/icons/profile.svg';
+
 const Tab = createBottomTabNavigator();
+
 const TabNavigator = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+
+        tabBarIcon: ({ focused }) => {
+          const size = 34;
+          const activeColor = '#1f4d42'; // dark green
+          const inactiveColor = '#6B7F7A'; // light green
+
+          let IconComponent;
+
+          if (route.name === 'Home') IconComponent = HomeIcon;
+          if (route.name === 'Explore') IconComponent = ExploreIcon;
+          if (route.name === 'Saved') IconComponent = SavedIcon;
+          if (route.name === 'Profile') IconComponent = ProfileIcon;
+
+          return (
+            <IconComponent
+              width={size}
+              height={size}
+              fill={focused ? activeColor : inactiveColor}
+            />
+          );
+        },
+
+        tabBarActiveTintColor: '#1f4d42',
+        tabBarInactiveTintColor: '#6B7F7A',
+
+        tabBarStyle: {
+          height: 80,
+          backgroundColor: '#fff',
+        },
+
+        tabBarLabelStyle: {
+          fontSize: 16,
+          marginBottom: 6,
+        },
+      })}
+    >
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Explore" component={ExploreScreen} />
       <Tab.Screen name="Saved" component={SavedScreen} />

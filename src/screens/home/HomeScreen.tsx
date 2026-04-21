d5200db848b0931c5bad6712b9d99f004440eb8d
NajeebSayyed <najeebsayyed777@gmail.com>
2026-04-21 16:53:04 +0530

    feat(home): implement ingredient input with add button, chips UI, and generate recipe action

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,32 +1,77 @@
 import { View, Image } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store';
 import HeroSection from '../../components/common/HeroSection';
 
-const HomeScreen = ({}) => {
+import PrimaryButton from '../../components/common/PrimaryButton';
+import Divider from '../../components/common/Divider';
+import IngredientInput from '../../components/recipe/IngredientInput';
+
+const HomeScreen = () => {
+  const [input, setInput] = useState('');
+  const [ingredients, setIngredients] = useState<string[]>([]);
+
   const user = useSelector((state: RootState) => state.auth.user);
 
-  return (
-    <View className="flex-1 bg-white px-7 ">
-      {/* Header with logo and notification icon */}
+  //  Add ingredient
+  const addIngredient = () => {
+    const value = input.trim().toLowerCase();
+    if (!value) return;
 
+    // prevent duplicates
+    if (ingredients.includes(value)) {
+      setInput('');
+      return;
+    }
+
+    setIngredients(prev => [...prev, value]);
+    setInput('');
+  };
+
+  // Remove ingredient
+  const removeIngredient = (item: string) => {
+    setIngredients(prev => prev.filter(i => i !== item));
+  };
+
+  return (
+    <View className="flex-1 bg-white px-7">
+      {/* Logo */}
       <Image
         source={require('../../assets/images/appLogo.png')}
         className="w-40 h-40 mt-6 absolute"
       />
 
-      {/* Greeting message */}
+      {/* Greeting */}
       <HeroSection
-        classname="mt-48 mb-8 "
+        classname="mt-48 mb-8"
         title={`Hello, ${
-          user?.user_metadata?.full_name.split(' ')[0] || 'Foodie'
-        }👋`}
-        subtitle="Whats in your kitchen ?"
+          user?.user_metadata?.full_name?.split(' ')[0] || 'Foodie'
+        } 👋`}
+        subtitle="What's in your kitchen?"
         subtitleStyle="text-lg font-nunitoSemiBold"
       />
 
-      {/* User Input */}
+      {/* Input + Plus */}
+
+      <IngredientInput
+        input={input}
+        setInput={setInput}
+        ingredients={ingredients}
+        addIngredient={addIngredient}
+        removeIngredient={removeIngredient}
+      />
+
+      {/* Generate Button */}
+      <PrimaryButton
+        title="Generate Recipe !"
+        onPress={() => {
+          console.log('Ingredients:', ingredients);
+        }}
+      />
+
+      {/* Divider */}
+      <Divider />
     </View>
   );
 };

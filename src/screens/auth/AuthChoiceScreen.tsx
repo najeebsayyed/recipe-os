385fbe7f1a27507dd1460d6e5f2ec03579e45f14
NajeebSayyed <najeebsayyed777@gmail.com>
2026-04-24 17:17:24 +0530

    feat(auth): update UI for auth choice screen

--- src/screens/auth/AuthChoiceScreen.tsx
+++ src/screens/auth/AuthChoiceScreen.tsx
@@ -1,6 +1,5 @@
-import { View } from 'react-native';
+import { Image, View } from 'react-native';
 import React from 'react';
-import HeroSection from '../../components/common/HeroSection';
 import PrimaryButton from '../../components/common/PrimaryButton';
 import Divider from '../../components/common/Divider';
 import SocialButton from '../../components/common/SocialButton';
@@ -10,31 +9,30 @@ import { setGuest } from '../../store/slices/authSlice';
 const AuthChoiceScreen = ({ navigation }: any) => {
   const dispatch = useDispatch();
   return (
-    <View className="flex-1 bg-white px-7 justify-between py-10">
+    <View className="flex-1 bg-white px-7 justify-center py-10">
       {/* Top Section */}
-      <View>
-        <HeroSection
-          title={'Welcome to RecipeOS 🍳'}
-          subtitle={'Cook smarter, eat better'}
+
+      <Image
+        source={require('../../assets/images/appLogo.png')}
+        className="h-20 w-24 self-center "
+      />
+
+      {/* Buttons */}
+      <View className="mt-10 gap-4">
+        <PrimaryButton
+          title="Log In"
+          onPress={() => navigation.navigate('Login')}
         />
 
-        {/* Buttons */}
-        <View className="mt-10 gap-4">
-          <PrimaryButton
-            title="Log In"
-            onPress={() => navigation.navigate('Login')}
-          />
-
-          <PrimaryButton
-            title="Sign Up"
-            onPress={() => navigation.navigate('Signup')}
-          />
-          <Divider label="Or" />
-          <SocialButton
-            title="Continue as Guest"
-            onPress={() => dispatch(setGuest())}
-          />
-        </View>
+        <PrimaryButton
+          title="Sign Up"
+          onPress={() => navigation.navigate('Signup')}
+        />
+        <Divider label="Or" className="mt-[-3]" />
+        <SocialButton
+          title="Continue as Guest"
+          onPress={() => dispatch(setGuest())}
+        />
       </View>
     </View>
   );

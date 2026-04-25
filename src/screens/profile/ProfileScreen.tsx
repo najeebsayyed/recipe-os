383e7f787860adbefabdf155bf672d29d63d603f
NajeebSayyed <najeebsayyed777@gmail.com>
2026-04-25 23:16:59 +0530

    improve profile  screen UI and layout

--- src/screens/profile/ProfileScreen.tsx
+++ src/screens/profile/ProfileScreen.tsx
@@ -4,22 +4,36 @@ import ScreenHeader from '../../components/common/ScreenHeader';
 import HeroSection from '../../components/common/HeroSection';
 import PrimaryButton from '../../components/common/PrimaryButton';
 import { useNavigation } from '@react-navigation/native';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store';
 
 const ProfileScreen = () => {
   const navigation = useNavigation<any>();
+  const user = useSelector((state: RootState) => state.auth.user);
   return (
     <View className="flex-1 bg-white px-7">
       <ScreenHeader title="Profile" />
-      <HeroSection
-        title="Guest User"
-        subtitle="Log in to save your recipes and access all features"
-      />
-      <PrimaryButton
-        title="Log In"
-        onPress={() => {
-          navigation.navigate();
-        }}
-      />
+
+      {user ? (
+        <HeroSection
+          title={user.user_metadata.full_name}
+          subtitle="Enjoy your recipe"
+        />
+      ) : (
+        <View>
+          {' '}
+          <HeroSection
+            title="Guest User"
+            subtitle="Log in to save your recipes and access all features"
+          />
+          <PrimaryButton
+            title="Log In"
+            onPress={() => {
+              navigation.navigate();
+            }}
+          />
+        </View>
+      )}
     </View>
   );
 };

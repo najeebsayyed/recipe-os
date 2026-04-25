a39235402727a190d6611e764b4d0384e51c4bd9
NajeebSayyed <najeebsayyed777@gmail.com>
2026-04-25 21:30:46 +0530

    ui: profile screen design completed

--- src/screens/profile/ProfileScreen.tsx
+++ src/screens/profile/ProfileScreen.tsx
@@ -1,11 +1,25 @@
 import { View } from 'react-native';
 import React from 'react';
 import ScreenHeader from '../../components/common/ScreenHeader';
+import HeroSection from '../../components/common/HeroSection';
+import PrimaryButton from '../../components/common/PrimaryButton';
+import { useNavigation } from '@react-navigation/native';
 
 const ProfileScreen = () => {
+  const navigation = useNavigation<any>();
   return (
     <View className="flex-1 bg-white px-7">
       <ScreenHeader title="Profile" />
+      <HeroSection
+        title="Guest User"
+        subtitle="Log in to save your recipes and access all features"
+      />
+      <PrimaryButton
+        title="Log In"
+        onPress={() => {
+          navigation.navigate();
+        }}
+      />
     </View>
   );
 };
